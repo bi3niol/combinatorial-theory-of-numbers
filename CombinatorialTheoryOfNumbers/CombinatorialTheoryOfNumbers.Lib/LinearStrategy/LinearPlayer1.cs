79da4e6edaaaf6d8103efd896b274220d98dafe3
kubmazur <jak.mazurkiewicz@gmail.com>
2019-05-04 21:53:25 +0200

    Added linear strategies for players 1 and 2

---
+++ CombinatorialTheoryOfNumbers/CombinatorialTheoryOfNumbers.Lib/LinearStrategy/LinearPlayer1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CombinatorialTheoryOfNumbers.Lib.LinearStrategy
+{
+    public class LinearPlayer1 : IPlayer1<int, int>
+    {
+        private int _lastIndex;
+
+        public LinearPlayer1()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            _lastIndex = 0;
+        }
+
+        public int Move(IGameState<int, int> gameState)
+        {
+            return _lastIndex++;
+        }
+    }
+}
