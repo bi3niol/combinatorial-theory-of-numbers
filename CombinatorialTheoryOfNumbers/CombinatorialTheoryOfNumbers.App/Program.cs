c0254b8d0e30ee5e67190edd3791e13a2a8c86ec
Mateusz Bieńkowski <bi3niol@gmail.com>
2019-05-05 02:14:13 +0200

    main

--- CombinatorialTheoryOfNumbers/CombinatorialTheoryOfNumbers.App/Program.cs
+++ CombinatorialTheoryOfNumbers/CombinatorialTheoryOfNumbers.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CombinatorialTheoryOfNumbers.Lib;
+using CombinatorialTheoryOfNumbers.Lib.LinearStrategy;
 using CombinatorialTheoryOfNumbers.Lib.RandomStrategy;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,13 @@ namespace CombinatorialTheoryOfNumbers.App
             IStatePresenter<int, int> presenter = new ColoredStatePresenter();
             List<IPlayer1<int, int>> P1s = new List<IPlayer1<int, int>>
             {
-                new RandomPlayer1(1)
+                new RandomPlayer1(10),
+                new LinearPlayer1()
             };
             List<IPlayer2<int, int>> P2s = new List<IPlayer2<int, int>>
             {
-                new RandomPlayer2(1)
+                new RandomPlayer2(100),
+                new LinearPlayer2()
             };
             List<LaunchConfig> configs = new List<LaunchConfig>()
             {
@@ -26,6 +29,7 @@ namespace CombinatorialTheoryOfNumbers.App
             {
                 foreach (var P2 in P2s)
                 {
+                    Console.WriteLine($"{P1.GetType().Name} vs {P2.GetType().Name}");
                     Game game = new Game(P1, P2, presenter);
                     foreach (var config in configs)
                     {
