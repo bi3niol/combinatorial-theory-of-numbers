41e7ca60a852affea5f43ea9a1ec23d020b7570e
Kornel Żaba <kornelzaba@gmail.com>
2019-05-18 17:59:15 +0200

    Added advanced player1 strat

--- CombinatorialTheoryOfNumbers/CombinatorialTheoryOfNumbers.Lib/AdvancedStrategy/AdvancedPlayer1.cs
+++ CombinatorialTheoryOfNumbers/CombinatorialTheoryOfNumbers.Lib/AdvancedStrategy/AdvancedPlayer1.cs
@@ -1,19 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CombinatorialTheoryOfNumbers.Lib.AdvancedStrategy
 {
     public class AdvancedPlayer1 : IPlayer1<int, int>
     {
+        private int _maxChosen = 0;
         public void Clear()
         {
-            //does nothing
+            _maxChosen = 0;
         }
 
         public int Move(IGameState<int, int> gameState)
         {
-            throw new NotImplementedException();
+            int maxLength = 0, chosenNumber = 0;
+            for(int i = 0; i < gameState.MaxGameLength && i < gameState.TargetSeriesLength * (_maxChosen + 1); i++)
+            {
+                if(gameState[i] == -1)
+                {
+                    int len = 0;
+                    for (int c = 0; c < gameState.PossibleColors; c++)
+                    {
+                        var coloredNumbers = gameState.GetColoredSubset(c);
+                        coloredNumbers.Add(i);
+                        len += Helpers.GetLengthOfLongestArithmeticSubsequence(coloredNumbers.ToList());
+                        if (len > maxLength)
+                        {
+                            maxLength = len;
+                            chosenNumber = i;
+                        }
+                    }
+                }
+            }
+            if(chosenNumber > _maxChosen)
+            {
+                _maxChosen = chosenNumber;
+            }
+            return chosenNumber;
         }
     }
 }
